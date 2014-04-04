60894e2ac9fdf5cfe3d9476e29a9d9e6268ef28b
Adam Hayward <adam@happy.cat>
2014-04-04 12:48:05 +0200

    Remove stub test code

--- tab-to-redis.py
+++ tab-to-redis.py
@@ -9,17 +9,9 @@ def tab_to_redis(handle):
         key, val = line.split("\t", 1)
         sys.stdout.write("*%d\r\n$%d\r\nSET\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n" % (3, 3, len(key), key, len(val), val))
 
-def test():
-    pass
 
 def main():
-    try:
-        if sys.argv[1] == "test":
-            test()
-            exit
-    except IndexError: pass
     tab_to_redis(sys.stdin)
-
 
 if __name__ == "__main__":
     main()
